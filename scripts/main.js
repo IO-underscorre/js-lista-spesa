6030058536e5c27367e2ea4688957bf607084334
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-11 16:44:57 +0200

    feat(javascript): script to print on page the shopping list completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
 const shoppingList = ['Pasta' , 'Carote', 'Mozzarella', 'Farro', 'Hummus', 'Latte', 'Cipolle', 'Patate'];
+
+const listContainer = document.getElementById('shopping-list');
+let i = 0;
+
+while(i < shoppingList.length) {
+    listContainer.innerHTML += `<li>${shoppingList[i]}</li>`;
+    i++;
+}
